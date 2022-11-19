b1a29dc471b628c37b90891b615559bddb290367
Mike Santos (RIT Student) <mjs8837@rit.edu>
2022-11-19 18:04:51 -0500

    Fixed all the picture prefabs as well as cleaning up photo manager code.

--- Assets/Scripts/PhotoManager.cs
+++ Assets/Scripts/PhotoManager.cs
@@ -15,27 +15,24 @@ public class PhotoManager : MonoBehaviour
 
     private List<GameObject> displayedPictures;
 
+    // Enum for type of pictures to display
     [SerializeField] enum PictureType { encouraging, neutral, calming, puppy };
     private PictureType currentPictureType = PictureType.encouraging;
 
-    [SerializeField] float switchTimer = 1.0f;
-    private float timeReset = 1.0f;
+    // Timer fields
+    float switchTimer = 10.0f;
+    private float timeReset = 10.0f;
 
-    [SerializeField] int currentPictureIndex = 0;
-
+    // Picture related fields
+    private int currentPictureIndex = 0;
     private float currentOpacity = 1.0f;
-
-    private float currentTimeHour;
-    private float currentTimeMinute;
-    private string currentTime;
-
-    [SerializeField] GameObject timeText;
 
     // Start is called before the first frame update
     void Start()
     {
         displayedPictures = new List<GameObject>();
 
+        // Initializing a list of prefabs based on the enum type
         switch (currentPictureType)
         {
             case PictureType.encouraging:
@@ -55,50 +52,55 @@ public class PhotoManager : MonoBehaviour
                 break;
         }
 
-        displayedPictures[currentPictureIndex].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        // Setting the first image to display
+        displayedPictures[currentPictureIndex].GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdatePhoto(timeReset);
-        SetCurrentTime();
     }
 
+    // Helper function to initialize a list of set prefabs
     private void InstantiateList(List<GameObject> prefabList)
     {
         for (int i = 0; i < prefabList.Count; i++)
         {
             GameObject tempPicture = Instantiate(prefabList[i]);
             displayedPictures.Add(tempPicture);
-            tempPicture.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+            tempPicture.GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, 0.0f);
         }
     }
 
     // Helper function to update photo timer and change background image
     private void UpdatePhoto(float timeReset)
     {
+        // Decreasing the timer before setting up to switch photos
         if (switchTimer > 0.0f)
         {
             switchTimer -= Time.deltaTime;
         }
         else
         {
+            // Decreasing opacity of the current photo and increasing the opacity of the next one to cyle through
             if (currentOpacity > 0.0f)
             {
                 currentOpacity -= Time.deltaTime / 4.0f;
 
                 if (currentPictureIndex < displayedPictures.Count - 1)
                 {
-                    displayedPictures[currentPictureIndex].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, currentOpacity);
-                    displayedPictures[currentPictureIndex + 1].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f - currentOpacity);           
+                    displayedPictures[currentPictureIndex].GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, currentOpacity);
+                    displayedPictures[currentPictureIndex + 1].GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, 1.0f - currentOpacity);           
                 }
                 else
                 {
-                    displayedPictures[0].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f - currentOpacity);
+                    displayedPictures[currentPictureIndex].GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, currentOpacity);
+                    displayedPictures[0].GetComponent<SpriteRenderer>().color = new Color(0.7f, 0.7f, 0.7f, 1.0f - currentOpacity);
                 }
             }
 
+            // Resetting opacity and timer to reset the cycle as well as increasing the current index
             else
             {
                 currentOpacity = 1.0f;
@@ -112,30 +114,5 @@ public class PhotoManager : MonoBehaviour
                 else { currentPictureIndex = 0; }
             }
         }
-    }
-
-    // Helper function to set the current time and display it on screen
-    private void SetCurrentTime()
-    {
-        if (DateTime.Now.Hour > 12)
-        {
-            currentTimeHour = DateTime.Now.Hour - 12;
-        }
-        else
-        {
-            currentTimeHour = DateTime.Now.Hour;
-        }
-
-        currentTimeMinute = DateTime.Now.Minute;
-
-        if (currentTimeMinute < 10)
-        {
-            currentTime = currentTimeHour.ToString() + ":0" + currentTimeMinute.ToString();
-        }
-        else
-        {
-            currentTime = currentTimeHour.ToString() + ":" + currentTimeMinute.ToString();
-        }      
-        timeText.GetComponent<TextMeshPro>().text = currentTime;
     }
 }
