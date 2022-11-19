59df748803dc1ccabb658399b598cd2fd42760e9
Mike Santos (RIT Student) <mjs8837@rit.edu>
2022-11-19 14:01:29 -0500

    Pushing more work with image cycling. Need to get prefabs for it to work properly.

--- Assets/Scripts/PhotoManager.cs
+++ Assets/Scripts/PhotoManager.cs
@@ -4,21 +4,79 @@ using UnityEngine;
 
 public class PhotoManager : MonoBehaviour
 {
-    [SerializeField] List<Sprite> productivePictures;
-    [SerializeField] List<Sprite> calmingPictures;
-    [SerializeField] List<Sprite> puppyPictures;
+    [SerializeField] List<GameObject> encouragingPictures;
+    [SerializeField] List<GameObject> neutralPictures;
+    [SerializeField] List<GameObject> calmingPictures;
+    [SerializeField] List<GameObject> puppyPictures;
+
+    private GameObject currentImage;
+
+    [SerializeField] enum PictureType { encouraging, neutral, calming, puppy };
+    private PictureType currentPicture = PictureType.encouraging;
+
+    [SerializeField] float switchTimer = 10.0f;
+    private float timeReset = 10.0f;
+
+    private int pictureCount = 0;
+    private int currentPictureIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        productivePictures = new List<Sprite>();
-        calmingPictures = new List<Sprite>();
-        puppyPictures = new List<Sprite>();
+        encouragingPictures = new List<GameObject>();
+        neutralPictures = new List<GameObject>();
+        calmingPictures = new List<GameObject>();
+        puppyPictures = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePhoto(timeReset);
+        currentImage = GameObject.Find("CurrentImage");
+    }
+
+    // Helper function to reset 
+    private void UpdatePhoto(float timeReset)
+    {
+        if (switchTimer > 0.0f)
+        {
+            switchTimer -= Time.deltaTime;
+        }
+        else
+        {
+            // Code to reset photo here based on photo type
+            switch(currentPicture)
+            {
+                case PictureType.encouraging:
+
+                    // Code for displaying encouraging pictures
+                    currentImage = encouragingPictures[currentPictureIndex];
+                    pictureCount = encouragingPictures.Count;
+
+                    if (currentPictureIndex < pictureCount)
+                    {
+                        currentPictureIndex++;
+                    }
+                    else
+                    {
+                        currentPictureIndex = 0;
+                    }
+
+                    break;
+
+                case PictureType.neutral:
+                    // Code for displaying neutral pictures
+                    break;
+                case PictureType.calming:
+                    // Code for displaying calming pictures
+                    break;
+                case PictureType.puppy:
+                    // Code for displaying calming pictures
+                    break;
+            }
+
+            switchTimer = timeReset;
+        }
     }
 }
