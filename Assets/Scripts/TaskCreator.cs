4894d778be913bfd9cd5e7220158f4edf3571601
Mike Santos (RIT Student) <mjs8837@rit.edu>
2022-11-20 10:38:48 -0500

    Forgot to save the scene :).

--- Assets/Scripts/TaskCreator.cs
+++ Assets/Scripts/TaskCreator.cs
@@ -31,6 +31,11 @@ public class TaskCreator : MonoBehaviour
     int startMinuteIndex;
     int endMinuteIndex;
 
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
